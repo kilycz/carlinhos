2615521275aeee4e2522302ea3159894c55e539b
kilycz <142522029+kilycz@users.noreply.github.com>
2024-10-30 11:02:42 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -5,7 +5,31 @@
 //chamada
 
 criaCartao(
-'Programação'
-'O que é JavaScript?'
+'Programação',
+'O que é JavaScript?',
 'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Culinaria',
+    'Como faz bolo de cenoura?',
+    'Com cenoura:)'
 )
